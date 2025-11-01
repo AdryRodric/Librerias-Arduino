ac68c04cb7ff2705e6717d13926a236845540aa2
AdryRodric <adryrodric@hotmail.com>
2025-11-01 08:58:15 -0500

    Prueba_matrizLED_8X8.ino

--- MATRIZ_LED_8X8/Prueba_matrizLED_8X8/Prueba_matrizLED_8X8.ino
+++ MATRIZ_LED_8X8/Prueba_matrizLED_8X8/Prueba_matrizLED_8X8.ino
@@ -1,39 +1,3 @@
-// #define DIN 13
-// #define CS  12
-// #define CLK 14
-
-// void enviar(uint8_t dir, uint8_t val) {
-//   digitalWrite(CS, LOW);
-//   shiftOut(DIN, CLK, MSBFIRST, dir);
-//   shiftOut(DIN, CLK, MSBFIRST, val);
-//   digitalWrite(CS, HIGH);
-// }
-
-// void setup() {
-//   pinMode(DIN, OUTPUT);
-//   pinMode(CS, OUTPUT);
-//   pinMode(CLK, OUTPUT);
-//   digitalWrite(CS, HIGH);
-
-//   enviar(0x0F, 0x00);
-//   enviar(0x0C, 0x01);
-//   enviar(0x0B, 0x07);
-//   enviar(0x09, 0x00);
-//   enviar(0x0A, 0x08);
-
-//   // Encender solo la fila 1
-//   enviar(1, B10000001);
-//   enviar(2, B01000010);
-//   enviar(3, B00100100);
-//   enviar(4, B00011000);
-//   enviar(5, B00100100);
-//   enviar(6, B01000010);
-//   enviar(7, B10000001);
-//   enviar(8, B00000000);
-// }
-
-// void loop() {}
-
 #include "MATRIZ_LED_8X8.h"
 
 #define DIN 13
@@ -48,25 +12,24 @@ void setup() {
 }
 
 void loop() {
+  // Animación de barrido desde la el primer led haste el último iluminando todo
   for(uint8_t columna = 0; columna <= 8; columna++){
     for(uint8_t fila = 0; fila <= 8; fila ++){
-      // matriz.LIMPIAR();
       matriz.SET_LED(columna,fila,true); matriz.MOSTRAR();
       delay(15);
     }
   }
-
+// Animación que hace lo contrario a la anumación anterior
   for(int8_t columna = 8; columna >= 0; columna --){
     for(int8_t fila = 8; fila >= 0; fila --){
-      // matriz.LIMPIAR();
       matriz.SET_LED(columna,fila,false); matriz.MOSTRAR();
       delay(15);
     }
   }
-
-  matriz.ENCENDER_TODOS(); delay(1000);
-  matriz.APAGAR_TODOS(); delay(500);
-  matriz.patronX(); delay(1000);
+// Patrones predeterninados
+  matriz.ENCENDER_TODOS(); delay(1000); // Para encender todos los leds de la matriz
+  matriz.APAGAR_TODOS(); delay(500); // Para apagar todos los leds
+  matriz.patronX(); delay(1000); 
   matriz.patronCUAD(); delay(1000);
   matriz.patronTRI(); delay(1000);
   matriz.patronCIR(); delay(1000);
